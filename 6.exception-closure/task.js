dc527b87574bbe9ffb06831717c1063fa819ecb7
Mariya Britvina <m.sea7@mail.ru>
2024-10-31 00:57:30 +0300

    Д/З по теме Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(str) {
+	const num = Number.parseFloat(str);
+	if (isNaN(num)) {
+		throw new Error("Невалидное значение");
+	}
+	return num;
+}
+
+function validateCount(str) {
+	try {
+		return parseCount(str);
+	} catch (e) {
+		return e;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b < c || b + c < a || c + a < b) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		return parseFloat((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3)));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (e) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}
